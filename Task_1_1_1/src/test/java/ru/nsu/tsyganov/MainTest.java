ebfae070a3da4e44df96ed89ba1b778d044f6c90
Alexander Tsyganov <a.tsyganov2@g.nsu.ru>
2024-09-10 23:23:59 +0700

    Added new tests

--- Task_1_1_1/src/test/java/ru/nsu/tsyganov/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/tsyganov/MainTest.java
@@ -42,11 +42,17 @@ class MainTest {
     }
 
     @Test
-    void checkDuplicates() {
-        int[] expected = {1, 1, 2, 3, 3};
-        int[] numbers = {3, 1, 2, 3, 1};
-
-        Main.heapsort(numbers);
-        assertArrayEquals(expected, numbers);
+    void checkPrint() {
+        int[] numbers = {1, 3, 4, 5, 2};
+        Main.printArray(numbers);
     }
+
+//    @Test
+//    void checkDuplicates() {
+//        int[] expected = {1, 1, 2, 3, 3};
+//        int[] numbers = {3, 1, 2, 3, 1};
+//
+//        Main.heapsort(numbers);
+//        assertArrayEquals(expected, numbers);
+//    }
 }
