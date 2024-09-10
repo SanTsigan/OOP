9dbe168a6f2f938305a2b3c3d025dd373ff93ec1
Alexander Tsyganov <a.tsyganov2@g.nsu.ru>
2024-09-10 20:56:11 +0700

    Added javadoc comments, fixed style mistakes, added shell-script

--- Task_1_1_1/src/main/java/ru/nsu/tsyganov/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/tsyganov/Main.java
@@ -1,14 +1,23 @@
 package ru.nsu.tsyganov;
 
+/**
+ * Класс Main
+ */
 public class Main {
-    public static void heapsort(int[] arr){
-        int N = arr.length;
+    /**
+     * Основная функция пирамидальной сортировки
+     * Сначала создаёт кучу затем достаём элементы и перемещаем в конец по одному и на каждом
+     * шаге строим максимальную кучу (max-heap) из оставшихся элементов
+     * @param arr
+     */
+    public static void heapsort(int[] arr) {
+        int n = arr.length;
 
-        for (int i = N / 2 - 1; i >= 0; i--) {
-            heapify(arr, N, i);
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
         }
 
-        for (int i = N - 1; i > 0; i--) {
+        for (int i = n - 1; i > 0; i--) {
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
@@ -17,7 +26,7 @@ public class Main {
         }
     }
 
-    static void heapify(int[] arr, int N, int i){
+    static void heapify(int[] arr, int N, int i) {
         int largest = i;
         int l = 2 * i + 1;
         int r = 2 * i + 2;
@@ -37,16 +46,15 @@ public class Main {
         }
     }
 
-    static void printArray(int[] arr){
+    static void printArray(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n; i++){
             System.out.print(arr[i]+" ");
-            }
+        }
         System.out.println();
     }
     public static void main(String[] args) {
         int[] arr = {5, 3, 4, 1, 2};
-        int n = arr.length;
 
         Main ob = new Main();
         ob.heapsort(arr);
