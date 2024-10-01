3037c6ce092a0c59315d0fa03f92bd22194de561
Alexander Tsyganov <a.tsyganov2@g.nsu.ru>
2024-10-01 11:01:02 +0700

    added class for tests

---
+++ Task_1_1_3/src/test/java/ru/nsu/tsyganov/expressions/MainTest.java
@@ -0,0 +1,12 @@
+package ru.nsu.tsyganov.expressions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void mainTest() {
+
+    }
+}
