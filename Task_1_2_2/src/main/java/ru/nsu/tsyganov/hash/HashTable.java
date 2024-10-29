f6cc536c489433878a427e38d718efa1fb5b5e75
Alexander Tsyganov <a.tsyganov2@g.nsu.ru>
2024-10-29 14:58:59 +0700

    first steps :)

--- Task_1_2_2/src/main/java/ru/nsu/tsyganov/hash/HashTable.java
+++ Task_1_2_2/src/main/java/ru/nsu/tsyganov/hash/HashTable.java
@@ -165,11 +165,4 @@ public class HashTable<K, V> {
             }
         }
     }
-
-    public static void main(String[] args) {
-        HashTable<String, Number> hashTable = new HashTable<>();
-        hashTable.put("one", 1);
-        hashTable.update("one", 1.0);
-        System.out.println(hashTable.get("one")); // Output: 1.0
-    }
 }
--- Task_1_2_2/src/main/java/ru/nsu/tsyganov/hash/Main.java
+++ Task_1_2_2/src/main/java/ru/nsu/tsyganov/hash/Main.java
@@ -2,6 +2,9 @@ package ru.nsu.tsyganov.hash;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashTable<String, Number> hashTable = new HashTable<>();
+        hashTable.put("one", 1);
+        hashTable.update("one", 1.0);
+        System.out.println(hashTable.get("one")); // Output: 1.0
     }
 }
