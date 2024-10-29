f65f9a8e07116f1950b3b4977cd9201ee9469062
Alexander Tsyganov <a.tsyganov2@g.nsu.ru>
2024-10-29 15:22:37 +0700

    fixing stuff...

---
+++ Task_1_1_3/src/main/java/ru/nsu/tsyganov/expressions/Number.java
@@ -0,0 +1,49 @@
+package ru.nsu.tsyganov.expressions;
+
+/**
+ * Класс для целых чисел.
+ */
+public class Number extends Expression {
+    private int value;
+
+    public Number(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public double eval(String variables) {
+        if (variables.isEmpty()) {
+            throw new RuntimeException("No variables");
+        }
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(value);
+    }
+
+    @Override
+    public Expression derivative(String var) {
+        if (var.isEmpty()) {
+            throw new RuntimeException("No variables");
+        }
+        return new Number(0);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        final Number other = (Number) obj;
+
+        return value == other.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value * 31;
+    }
+}
