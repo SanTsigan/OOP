0fcccdc7d15cb8921148aa7d31ed4bf6f2d7ff3d
SanTsigan <SanTsigan@users.noreply.github.com>
2024-11-04 15:35:21 +0000

    Deploying to gh-pages from @ SanTsigan/OOP@bdb0eb91a5e734241afc80f2472594be6d2bffd7 🚀

--- Task_1_2_2/type-search-index.js
+++ Task_1_2_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.tsyganov.hash","l":"HashTable"},{"p":"ru.nsu.tsyganov.hash","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.tsyganov.hash","l":"HashTable"}];updateSearchResults();
