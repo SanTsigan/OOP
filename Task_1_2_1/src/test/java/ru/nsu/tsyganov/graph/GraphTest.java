2e28e8ff4f61f326f57e0504df1bc8d43bdc72d6
Alexander Tsyganov <a.tsyganov2@g.nsu.ru>
2024-11-12 15:15:25 +0700

    changes

--- Task_1_2_1/src/test/java/ru/nsu/tsyganov/graph/GraphTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/tsyganov/graph/GraphTest.java
@@ -228,8 +228,8 @@ class GraphTest {
         Vertex<Integer> vertex3 = new Vertex<Integer>(3);
         Vertex<Integer> vertex4 = new Vertex<Integer>(4);
         Edge<Integer, Integer> edge12 = new Edge<Integer, Integer>(vertex1, vertex2, 12, (double)1);
-        Edge<Integer, Integer> edge13 = new Edge<Integer, Integer>(vertex1, vertex3, 12, (double)1);
-        Edge<Integer, Integer> edge24 = new Edge<Integer, Integer>(vertex2, vertex4, 12, (double)1);
+        Edge<Integer, Integer> edge13 = new Edge<Integer, Integer>(vertex1, vertex3, 12, (double)2);
+        Edge<Integer, Integer> edge24 = new Edge<Integer, Integer>(vertex2, vertex4, 12, (double)3);
         integerGraph.addVertex(vertex1);
         integerGraph.addVertex(vertex2);
         integerGraph.addVertex(vertex3);
