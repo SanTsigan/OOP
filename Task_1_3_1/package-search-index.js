938c11171c686daf3f46c892640e9619bb36e815
SanTsigan <SanTsigan@users.noreply.github.com>
2024-11-18 09:00:31 +0000

    Deploying to gh-pages from @ SanTsigan/OOP@c37b6e17979f7c2d2548ca4d12dbbb4e31d6842d 🚀

--- Task_1_3_1/package-search-index.js
+++ Task_1_3_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.tsyganov.substringfinder"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.tsyganov.substringfinder"}]
--- Task_1_3_1/type-search-index.js
+++ Task_1_3_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.tsyganov.substringfinder","l":"SubstringFinder"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.tsyganov.substringfinder","l":"SubstringFinder"}]
